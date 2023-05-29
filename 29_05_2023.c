2b5fc247deab820d3a7fa82bad47eeac6d931d4e
Jameel Ahmed. J <123755027+Jameel-Ahmed-2003@users.noreply.github.com>
2023-05-29 10:32:06 +0530

    Add files via upload

---
+++ 29_05_2023.c
@@ -0,0 +1,22 @@
+/*
+The program accept N integers and should print 2 integers among them whose difference is the maximum
+*/
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    printf("n = ");
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    int min=a[0],max=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(max<a[i])
+        max=a[i];
+        if(min>a[i])
+        min=a[i];
+    }
+    printf("%d %d",min,max);
+}
